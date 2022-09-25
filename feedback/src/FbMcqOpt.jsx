e30ebcba617ee0a16fdd66c05e4dc0fd0459664a
Pushparaj Kavara <pushparajkavara1611@gmail.com>
2022-09-25 22:02:00 +0530

    complete feedback form with response stored in state

--- feedback/src/FbMcqOpt.jsx
+++ feedback/src/FbMcqOpt.jsx
@@ -1,12 +1,19 @@
 import './FbMcqOpt.css'
 
-function FbMcqOpt({ qNo, optNo, option }) {
+function FbMcqOpt({ qNo, optNo, option, res, onChangeHandler }) {
   return (
     <>
       <div className="flex-item flex-item-opts">
         <div className="flex">
           <span className="flex-item flex-1">
-            <input id={`q${qNo}-opt${optNo}`} type="radio" name={`ques${qNo}`} value={option} />
+            <input
+              id={`q${qNo}-opt${optNo}`}
+              type="radio"
+              name={`ques${qNo}`}
+              value={option}
+              checked={res === option ? true : false}
+              onChange={() => onChangeHandler(option, qNo)}
+            />
           </span>
           <span className="flex-item flex-11">
             <label htmlFor={`q${qNo}-opt${optNo}`}>{option}</label>
