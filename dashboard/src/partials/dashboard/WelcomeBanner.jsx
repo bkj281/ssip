332bdcc72bdb3c375626220e2e628ec690c17699
bkj281 <66874987+bkj281@users.noreply.github.com>
2022-10-06 02:11:37 +0530

    Updated Welcome Banner

--- dashboard/src/partials/dashboard/WelcomeBanner.jsx
+++ dashboard/src/partials/dashboard/WelcomeBanner.jsx
@@ -48,8 +48,8 @@ function WelcomeBanner() {
 
       {/* Content */}
       <div className="relative">
-        <h1 className="text-2xl md:text-3xl text-slate-800 font-bold mb-1">Good afternoon, Acme Inc. 👋</h1>
-        <p>Here is what’s happening with your projects today:</p>
+        <h1 className="text-2xl md:text-3xl text-slate-800 font-bold mb-1">Greetings of the Day, Officer. 👋</h1>
+        <p>Here is what people felt about Police Stations.</p>
       </div>
 
     </div>
