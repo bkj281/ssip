b4887f96c4e5e721c5d4ba80b048d00780d63d0b
Nirmal-Mudaliar <nirmalmudaliar22@gmail.com>
2022-10-06 19:25:59 +0530

    Implemented get rating count for stationId, district, subDivision route

--- server/feedback/urls.py
+++ server/feedback/urls.py
@@ -1,7 +1,9 @@
-from .views import FilterFeedback, form
+from .views import FilterFeedback, GetRatingCount, form
 from django.urls import path
 
 urlpatterns = [
     path("form/", form.as_view(), name="Feedback Form"),
     path("filter/", FilterFeedback.as_view(), name="Feedback Form"),
+    path("rating/count/", GetRatingCount.as_view(), name="Feedback Form"),
+
 ]
--- server/feedback/views.py
+++ server/feedback/views.py
@@ -6,7 +6,7 @@ from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from .serializers import FeedbackSerializers
+from .serializers import FeedbackSerializers, RatingCountSerializer
 from .models import responseModel
 from verification.models import phoneModel
 
@@ -254,3 +254,64 @@ class FilterFeedback(APIView):
 
         except:
             return Response(data={'message': 'Unable to access the data'}, status=status.HTTP_400_BAD_REQUEST)
+
+class GetRatingCount(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        response = request.data
+
+        try:
+            district = response["district"]
+            subdivision = response["subdivision"]
+            station_id = response["station_id"]
+
+            if station_id == "" and district == "" and subdivision == "":
+                q = "SELECT id, res4, COUNT(*) AS count FROM feedback_responsemodel GROUP BY res4"
+                queryset = responseModel.objects.raw(q)
+                serializer = RatingCountSerializer(queryset, many=True)
+                print(serializer.data)
+                return Response(
+                    serializer.data,
+                    status=status.HTTP_200_OK,
+                )
+
+            if station_id != "" and district == "" and subdivision == "":
+                q = "SELECT id, res4, COUNT(*) AS count FROM feedback_responsemodel WHERE feedback_responsemodel.station_id =" + "'" + station_id +"' GROUP BY res4"
+                queryset = responseModel.objects.raw(q)
+                serializer = RatingCountSerializer(queryset, many=True)
+                print(serializer.data)
+                return Response(
+                    serializer.data,
+                    status=status.HTTP_200_OK,
+                )
+            
+            if station_id == "" and district != "" and subdivision == "":
+                q = "SELECT id, res4, COUNT(*) AS count FROM feedback_responsemodel WHERE feedback_responsemodel.district =" + "'" + district +"' GROUP BY res4"
+                queryset = responseModel.objects.raw(q)
+                serializer = RatingCountSerializer(queryset, many=True)
+                print(serializer.data)
+                return Response(
+                    serializer.data,
+                    status=status.HTTP_200_OK,
+                )
+
+            if station_id == "" and district == "" and subdivision != "":
+                q = "SELECT id, res4, COUNT(*) AS count FROM feedback_responsemodel WHERE feedback_responsemodel.subdivision =" + "'" + subdivision +"' GROUP BY res4"
+                queryset = responseModel.objects.raw(q)
+                serializer = RatingCountSerializer(queryset, many=True)
+                print(serializer.data)
+                return Response(
+                    serializer.data,
+                    status=status.HTTP_200_OK,
+                )
+
+        except KeyError as e:
+            return Response(
+                data={
+                    'message': f'{e} is not provided'
+                },
+                status=status.HTTP_404_NOT_FOUND,
+            )
+
+        
